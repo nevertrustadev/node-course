48d2651dc2b5c66452ea07243c49c30a80fa2632
lampertc <christopherlampert@rentalcars.com>
2020-01-31 11:53:19 +0000

    added views and templating engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,36 @@
 const path = require('path');
 const express = require('express');
+
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Welcome!',
+		name: 'Chris Lampert'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About Me',
+		name: 'Chris Lampert'
+	});
+});
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		title: 'Help Page',
+		message: 'Welcome to the help page - how can we help?'
+	})
+});
+
+
+
 
 app.get('/weather', (req, res) => {
 	res.send({
