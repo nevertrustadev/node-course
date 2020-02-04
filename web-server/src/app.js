c891ef692f5d6f824b5ab6a2d777f218c6bdf863
lampertc <christopherlampert@rentalcars.com>
2020-02-04 17:14:40 +0000

    added partial templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,21 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 
@@ -25,6 +36,7 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
 	res.render('help', {
 		title: 'Help Page',
+		name: 'Chris Lampert',
 		message: 'Welcome to the help page - how can we help?'
 	})
 });
