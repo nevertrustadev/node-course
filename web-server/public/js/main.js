fbab4bd15fc82b1c4ce5c9ab0a71a052cbea1957
lampertc <christopherlampert@rentalcars.com>
2020-02-24 08:32:59 +0000

    created front end form for weather service

--- web-server/public/js/main.js
+++ web-server/public/js/main.js
@@ -1,1 +1,18 @@
-console.log('Client JavaScript file is loaded!');
+const weatherForm = document.querySelector('form');
+const searchInput = document.getElementById('search');
+
+weatherForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const location = searchInput.value;
+
+	fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+		response.json().then((data) => {
+			if (data.error) {
+				console.log(data.error);
+			} else {
+				console.log(data.location, data.forecast)
+			}
+		})
+	});
+});
