31e1723e44ba8cf8109387ac0cc04081a65fb0be
lampertc <christopherlampert@rentalcars.com>
2020-02-26 17:21:03 +0000

    made the app production ready

--- web-server/public/js/main.js
+++ web-server/public/js/main.js
@@ -3,8 +3,6 @@ const searchInput = document.getElementById('search');
 const messageOne = document.getElementById('message-1');
 const messageTwo = document.getElementById('message-2');
 
-
-
 weatherForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 
@@ -12,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
 	messageOne.textContent = 'finding results...';
 	messageTwo.textContent = '';
 
-	fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+	fetch('/weather?address=' + location).then((response) => {
 		response.json().then((data) => {
 			if (data.error) {
 				messageOne.textContent = data.error;
