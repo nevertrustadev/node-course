a4ddc449a71aa8f773427fa8c670290e9581788b
lampertc <christopherlampert@rentalcars.com>
2020-05-20 12:54:22 +0100

    added mongo stuff

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,19 @@
 // CRUD Create Read, Update, Delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const {MongoClient, ObjectID} = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id.id);
+console.log(id.getTimestamp());
+
+
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client)  => {
 
@@ -44,28 +53,28 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 	// })
 
 
-	db.collection('tasks').insertMany([
-		{
-			description: 'Buy milk',
-			completed: true
-		},
-		{
-			description: 'Buy bread',
-			completed: true
-		},
-		{
-			description: 'Buy toilet roll',
-			completed: false
-		}
-	], (error, result) => {
-
-		if (error) {
-			return console.log('Could not insert tasks!');
-		}
-
-		console.log(result.ops);
-
-
-	})
+	// db.collection('tasks').insertMany([
+	// 	{
+	// 		description: 'Buy milk',
+	// 		completed: true
+	// 	},
+	// 	{
+	// 		description: 'Buy bread',
+	// 		completed: true
+	// 	},
+	// 	{
+	// 		description: 'Buy toilet roll',
+	// 		completed: false
+	// 	}
+	// ], (error, result) => {
+	//
+	// 	if (error) {
+	// 		return console.log('Could not insert tasks!');
+	// 	}
+	//
+	// 	console.log(result.ops);
+	//
+	//
+	// })
 });
 
