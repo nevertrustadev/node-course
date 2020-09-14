1a2325d0a40fd859ae6499ae4517e747026307e2
lampertc <christopherlampert@rentalcars.com>
2020-09-14 12:15:46 +0100

    added bcrypt

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -13,3 +13,24 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is up on ' + port);
 });
+
+
+
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+    const password = "sausage69";
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password);
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare('sausage69', hashedPassword);
+
+    console.log(isMatch)
+};
+
+
+
+
+myFunction()
