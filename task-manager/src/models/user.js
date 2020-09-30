0ddadeb272608548ea8a101bef0858638b8a6b73
lampertc <christopherlampert@rentalcars.com>
2020-09-30 12:55:31 +0100

    added auth tokens

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
 	name: {
@@ -42,23 +43,41 @@ const userSchema = new mongoose.Schema({
 				throw new Error('Password must NOT contain the word password')
 			}
 		}
-	}
+	},
+
+	tokens: [{
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 });
+
+userSchema.methods.generateAuthToken = async function () {
+	const user = this;
+	const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse');
+
+	user.tokens = user.tokens.concat({ token });
+
+	await user.save()
+
+	return token;
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
 	
 	const user = await User.findOne({ email })
-	const isMatch = await bcrypt.compare(password, user.password)
 
 	if (!user) {
 		throw new Error('Unable to login!');
 	}
 
+	const isMatch = await bcrypt.compare(password, user.password)
 
 	if (!isMatch) {
 		throw new Error('Unable to login!');
 	}
-
+	
 	return user;
 
 }
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -9,7 +9,9 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body);
     try {
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken()
+
+        res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e);
     }
@@ -18,11 +20,12 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
 
-        res.send(user);
+        res.send({ user, token });
 
     } catch(e) {
-        res.status(400).send(e);
+        res.status(400).send();
     }
 });
 
